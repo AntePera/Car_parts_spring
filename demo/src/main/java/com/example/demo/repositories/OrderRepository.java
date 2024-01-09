f68e593f147c9a15024ac44c195446f51e4ef381
AntePera <92494174+AntePera@users.noreply.github.com>
2024-01-09 22:52:13 +0100

    Delete demo directory

--- demo/src/main/java/com/example/demo/repositories/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.Order;
-
-public interface OrderRepository extends JpaRepository<Order,Integer>{
-    
-}
